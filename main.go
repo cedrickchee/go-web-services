cb4b4dcb91ff1002af72592b914cfa5d5af8e8ab
Cedric Chee <cedric@invictusbyte.com>
2021-03-09 21:19:43 +0800

    add a simple error handling package

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	if 1 == 2 {
+		return errors.New("random error")
+	}
+
+	return nil
+}
