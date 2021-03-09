72469d79c0d8d7e6c8ea5cdb213a7104dfb94652
Cedric Chee <cedric@invictusbyte.com>
2021-03-09 22:29:04 +0800

    add another dependency for httptreemux

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cedrickchee/gowebservices
 
 go 1.16
 
-require github.com/pkg/errors v0.9.1
+require (
+	github.com/dimfeld/httptreemux/v5 v5.2.2
+	github.com/pkg/errors v0.9.1
+)
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/dimfeld/httptreemux/v5"
 	"github.com/pkg/errors"
 )
 
@@ -18,6 +20,9 @@ func run() error {
 	if 1 == 2 {
 		return errors.New("random error")
 	}
+
+	m := httptreemux.NewContextMux()
+	m.Handle(http.MethodGet, "/test", nil)
 
 	return nil
 }
