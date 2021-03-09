36d47bef79b2bf729e4babec82084b3e4a4a8c9a
Cedric Chee <cedric@invictusbyte.com>
2021-03-09 23:39:49 +0800

    add logging

---
+++ app/sales-api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	log := log.New(os.Stdout, "SALES : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+	if err := run(log); err != nil {
+		log.Println("main: error:", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *log.Logger) error {
+
+	return nil
+}
