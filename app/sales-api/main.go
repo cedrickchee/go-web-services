bcf8e59fbc0057e701c45f4425e795db327815cb
Cedric Chee <cedric@invictusbyte.com>
2021-03-10 16:22:32 +0800

    routing: create a new handlers package
    We now got some mux that we can bind some routes to.

--- app/sales-api/main.go
+++ app/sales-api/main.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/ardanlabs/conf"
+	"github.com/cedrickchee/gowebservices/app/sales-api/handlers"
 	"github.com/pkg/errors"
 )
 
@@ -113,8 +114,8 @@ func run(log *log.Logger) error {
 	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
 
 	api := http.Server{
-		Addr: cfg.Web.APIHost,
-		// Handler:      handlers.API(build, shutdown, log, auth, db),
+		Addr:         cfg.Web.APIHost,
+		Handler:      handlers.API(build, shutdown, log),
 		ReadTimeout:  cfg.Web.ReadTimeout,
 		WriteTimeout: cfg.Web.WriteTimeout,
 	}
