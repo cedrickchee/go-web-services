56dfe61cc2b9ae10eefef04110a330eacf561636
Cedric Chee <cedric@invictusbyte.com>
2021-03-11 17:08:50 +0800

    feat (foundation): sending responses
    Abstract how we are sending responses & handling requests.

--- app/sales-api/handlers/check.go
+++ app/sales-api/handlers/check.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
 	"log"
+	"math/rand"
 	"net/http"
+
+	"github.com/cedrickchee/gowebservices/foundation/web"
 )
 
 type check struct {
@@ -12,11 +15,15 @@ type check struct {
 }
 
 func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		return errors.New("untrusted error")
+	}
+
 	status := struct {
 		Status string
 	}{
 		Status: "OK",
 	}
 	c.log.Println(r, status)
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, http.StatusOK)
 }
