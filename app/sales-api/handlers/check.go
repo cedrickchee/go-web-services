7c748b3d94405c388c724767590d250d0961b46f
Cedric Chee <cedric@invictusbyte.com>
2021-03-14 23:58:13 +0800

    fix: remove random request error

--- app/sales-api/handlers/check.go
+++ app/sales-api/handlers/check.go
@@ -2,9 +2,7 @@ package handlers
 
 import (
 	"context"
-	"errors"
 	"log"
-	"math/rand"
 	"net/http"
 
 	"github.com/cedrickchee/gowebservices/foundation/web"
@@ -15,10 +13,6 @@ type check struct {
 }
 
 func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	if n := rand.Intn(100); n%2 == 0 {
-		return web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
-	}
-
 	status := struct {
 		Status string
 	}{
