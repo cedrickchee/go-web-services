6e72889fb9b2828e0a7f181728f06b6dc30efc4d
Cedric Chee <cedric@invictusbyte.com>
2021-03-16 21:06:25 +0800

    fix: make seed data user_id value same as test data

--- business/data/schema/seed.go
+++ business/data/schema/seed.go
@@ -28,13 +28,13 @@ func Seed(db *sqlx.DB) error {
 const seeds = `
 -- Create admin and regular User with password "sup3rS3cr3tGolang"
 INSERT INTO users (user_id, name, email, roles, password_hash, date_created, date_updated) VALUES
-	('5cf37266-3473-4006-984f-9325122678b7', 'Admin', 'admin@example.com', '{ADMIN,USER}', '$2a$10$1ggfMVZV6Js0ybvJufLRUOWHS5f6KneuP0XwwHpJ8L8ipdry9f2/a', '2019-03-24 00:00:00', '2019-03-24 00:00:00'),
-	('45b5fbd3-755f-4379-8f07-a58d4a30fa2f', 'User', 'user@example.com', '{USER}', '$2a$10$9/XASPKBbJKVfCAZKDH.UuhsuALDr5vVm6VrYA9VFR8rccK86C1hW', '2019-03-24 00:00:00', '2019-03-24 00:00:00')
+	('1110d50a-7cea-4279-9f7d-b79ed659d31b', 'Admin', 'admin@example.com', '{ADMIN,USER}', '$2a$10$1ggfMVZV6Js0ybvJufLRUOWHS5f6KneuP0XwwHpJ8L8ipdry9f2/a', '2019-03-24 00:00:00', '2019-03-24 00:00:00'),
+	('dadd74d1-55f6-4d3b-b203-b2898464ac91', 'User', 'user@example.com', '{USER}', '$2a$10$9/XASPKBbJKVfCAZKDH.UuhsuALDr5vVm6VrYA9VFR8rccK86C1hW', '2019-03-24 00:00:00', '2019-03-24 00:00:00')
 	ON CONFLICT DO NOTHING;
 
 INSERT INTO products (product_id, user_id, name, cost, quantity, date_created, date_updated) VALUES
-	('a2b0639f-2cc6-44b8-b97b-15d69dbb511e', '45b5fbd3-755f-4379-8f07-a58d4a30fa2f', 'Bag', 50, 42, '2019-01-01 00:00:01.000001+00', '2019-01-01 00:00:01.000001+00'),
-	('72f8b983-3eb4-48db-9ed0-e45cc6bd716b', '45b5fbd3-755f-4379-8f07-a58d4a30fa2f', 'Shoes', 75, 120, '2019-01-01 00:00:02.000001+00', '2019-01-01 00:00:02.000001+00')
+	('a2b0639f-2cc6-44b8-b97b-15d69dbb511e', 'dadd74d1-55f6-4d3b-b203-b2898464ac91', 'Bag', 50, 42, '2019-01-01 00:00:01.000001+00', '2019-01-01 00:00:01.000001+00'),
+	('72f8b983-3eb4-48db-9ed0-e45cc6bd716b', 'dadd74d1-55f6-4d3b-b203-b2898464ac91', 'Shoes', 75, 120, '2019-01-01 00:00:02.000001+00', '2019-01-01 00:00:02.000001+00')
 	ON CONFLICT DO NOTHING;
 
 INSERT INTO sales (sale_id, product_id, quantity, paid, date_created) VALUES
