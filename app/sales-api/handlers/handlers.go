601f94439a238e9caa1d81df67e08b097218769f
Cedric Chee <cedric@invictusbyte.com>
2021-03-17 15:26:01 +0800

    feat (app): define & bind user handlers to router

--- app/sales-api/handlers/handlers.go
+++ app/sales-api/handlers/handlers.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/cedrickchee/gowebservices/business/auth"
+	"github.com/cedrickchee/gowebservices/business/data/user"
 	"github.com/cedrickchee/gowebservices/business/mid"
 	"github.com/cedrickchee/gowebservices/foundation/web"
 	"github.com/jmoiron/sqlx"
@@ -24,6 +25,19 @@ func API(build string, shutdown chan os.Signal, log *log.Logger, a *auth.Auth, d
 
 	app.Handle(http.MethodGet, "/readiness", cg.readiness)
 	app.Handle(http.MethodGet, "/liveness", cg.liveness)
+
+	// Register user management and authentication endpoints.
+	ug := userGroup{
+		user: user.New(log, db),
+		auth: a,
+	}
+
+	app.Handle(http.MethodGet, "/users/:page/:rows", ug.query, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodGet, "/users/:id", ug.queryByID, mid.Authenticate(a))
+	app.Handle(http.MethodGet, "/users/token/:kid", ug.token)
+	app.Handle(http.MethodPost, "/users", ug.create, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodPut, "/users/:id", ug.update, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodDelete, "/users/:id", ug.delete, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
 
 	return app
 }
